00bc2896dff72b604777b4604d985adfcd04600c
Kartik Bhatt <kartik200421@gmail.com>
2025-01-09 19:50:38 +0530

    refactor - responsive

--- components/ui/button2.tsx
+++ components/ui/button2.tsx
@@ -1,7 +1,7 @@
 "use client"
 export default function Button({ value }: { value: string }) {
   return (
-    <button className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-semibold py-2 px-3 rounded-xl shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+    <button className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-semibold px-2 py-1 rounded-xl shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
       {value}
     </button>
   )
